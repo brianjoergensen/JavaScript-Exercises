f4d753a75ec13545a872867513488bd8a53b8fe1
brianjoergensen <brianjoe@live.dk>
2017-10-08 16:27:08 +0200

    init

---
+++ custom.js
@@ -0,0 +1,19 @@
+
+let firstName = document.getElementById('firstname');
+let lastName = document.getElementById('lastname');
+
+function validate() {
+    console.log(inputFields.value);
+}
+
+
+
+
+
+// function validate() {
+//     if(typeof allInputs >= 2) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
