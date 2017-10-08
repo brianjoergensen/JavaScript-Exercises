3c0920784ca59a9ac9055043c200c6c52eb018b5
brianjoergensen <brianjoe@live.dk>
2017-10-08 23:13:53 +0200

    shoppinglist

--- custom.js
+++ custom.js
@@ -1,19 +1,31 @@
 
-let firstName = document.getElementById('firstname');
-let lastName = document.getElementById('lastname');
-
-function validate() {
-    console.log(inputFields.value);
-}
-
-
-
-
+// let firstName = document.getElementById('firstname');
+// let lastName = document.getElementById('lastname');
 
 // function validate() {
-//     if(typeof allInputs >= 2) {
-//         console.log(true);
-//     } else {
-//         console.log(false);
-//     }
+//     console.log(inputFields.value);
 // }
+
+// const myArray = ['Hello', 'how', 'are', 'you', '?'];
+// const myArrayJoin = myArray.join('&nbsp;').toString();
+// document.getElementById('shoppingList').innerHTML = myArrayJoin;
+
+// myArray.forEach(function(myArray) {
+//     document.getElementById('shoppingList').innerHTML += '<li>' + myArray + '</li>';
+//     // console.log(myArray);
+// });
+
+const shoppingItems = [];
+
+function displayShoppingItems() {
+    const itemInputField = document.getElementById('shoppingListInput').value;
+
+    if(itemInputField == '') {
+        return false;
+    }
+
+    shoppingItems.push(itemInputField);
+
+    document.getElementById('shoppingList').innerHTML += '<li>'+shoppingItems[shoppingItems.length-1]+'</li>';
+    console.log(shoppingItems[shoppingItems.length-1]);
+}
